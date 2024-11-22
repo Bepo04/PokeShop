b85acb36d600b97bb12fae04fd92d168a75e53a4
Eric Ayxendri <2023_eric.ayxendri@iticbcn.cat>
2024-11-22 18:41:31 +0100

    Added Login

--- app/src/main/java/com/ericandpau/pokeshop/BottomNav.java
+++ app/src/main/java/com/ericandpau/pokeshop/BottomNav.java
@@ -8,21 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BottomNav#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class BottomNav extends Fragment {
 
     public BottomNav() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +24,6 @@ public class BottomNav extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_bottom_nav, container, false);
     }
 }
