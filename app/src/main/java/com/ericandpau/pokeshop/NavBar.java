46a4fde8ef7ab6e5d312a74cec8f3fb697146e12
Eric Ayxendri <2023_eric.ayxendri@iticbcn.cat>
2024-12-01 15:58:50 +0100

    fent

--- app/src/main/java/com/ericandpau/pokeshop/NavBar.java
+++ app/src/main/java/com/ericandpau/pokeshop/NavBar.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class NavBar extends Fragment {
 
--- app/src/main/java/com/ericandpau/pokeshop/producte.java
+++ app/src/main/java/com/ericandpau/pokeshop/producte.java
@@ -8,22 +8,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class Producte extends Fragment {
+public class producte extends Fragment {
 
-    public Producte() {
-        // Required empty public constructor
+    public producte() {
+
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_producte, container, false);
     }
 }
